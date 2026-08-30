a70a9dc2df51d593398f2e57e2af8703d039a416
Orlando <galunggong14@gmail.com>
2026-08-29 21:33:39 -0700

    Add machines API routes

--- server/index.js
+++ server/index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send("AI assistance backend running");
 })
 
+app.get("/api/machines", (req, res) => {
+    res.send("Machine data will go here");
+})
+
 app.listen(5000, () => {
     console.log("server running on port 5000");
 });
