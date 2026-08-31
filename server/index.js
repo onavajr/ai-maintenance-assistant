ccedbb68aef393954049290218fe7299e05e7735
Orlando <galunggong14@gmail.com>
2026-08-30 19:03:16 -0700

    Add machine API routes

--- server/index.js
+++ server/index.js
@@ -6,9 +6,20 @@ app.get('/', (req, res) => {
     res.send("AI assistance backend running");
 })
 
+const machines = [{ id: 1, name: "Robot arm A"},
+        { id: 2, name: "Conveyor line 1"},];
+
 app.get("/api/machines", (req, res) => {
-    res.send("Machine data will go here");
+    res.json(machines);
 })
+
+app.get("/api/machines/:id", (req, res) => {
+    const machineId = Number(req.params.id);
+
+    const machine = machines.find((machine) => machine.id === machineId);
+    
+    res.json(machine);
+});
 
 app.listen(5000, () => {
     console.log("server running on port 5000");
