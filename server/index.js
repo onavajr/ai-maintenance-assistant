1abed57757ece4a97d41619a1265563df092d424
Orlando <galunggong14@gmail.com>
2026-09-04 21:26:55 -0700

    Set up MongoDB connection with Mongoose

--- server/index.js
+++ server/index.js
@@ -1,4 +1,6 @@
+require('dotenv').config();
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
@@ -76,6 +78,14 @@ app.patch("/api/machines/:id",(req, res) => {
 
     res.json(machine);
 });
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        console.log("Connected to MongoDB");
+    })
+    .catch((err) => {
+        console.log("Connection error:", err);
+    })
 
 app.listen(5000, () => {
     console.log("server running on port 5000");
